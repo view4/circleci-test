62fd3b14092f585352258e8c78af824020dc7d0c
Gabriel Morris <gabrielm@stork.inc>
2024-12-27 09:14:59 +0000

    update

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('BARFLY yeah:wq!'); 
+  res.write('BARFLY yeah:boy!'); 
   res.end(); 
 }).listen(8080, '0.0.0.0');
