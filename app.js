fdee9eedbf89a1f0a135e611e9b26cbfde04957d
Gabriel Morris <gabrielm@stork.inc>
2024-12-27 09:43:03 +0000

    update

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-  res.write('hello progress'); 
+  res.write('assured  progress'); 
   res.end(); 
 }).listen(8080, '0.0.0.0');
